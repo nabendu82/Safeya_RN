d4ddab1aa6356795a20091f23d6325c3f21afb6f
nabendu82 <nabendu.biswas@gmail.com>
2022-03-11 09:40:05 +0530

    Day 3

---
+++ DOM-Basics/main.js
@@ -0,0 +1,17 @@
+//Grab Single Element
+// const form = document.getElementById('my-form');
+// console.log(form);
+
+// console.log(document.querySelector('#my-form'));
+// console.log(document.querySelector('.container'));
+// console.log(document.querySelector('h1'));
+
+//Grab multiple elements
+const nodeItems = document.querySelectorAll('.item');
+const colItems = document.getElementsByClassName('item');
+// console.log(document.getElementsByTagName('li'));
+nodeItems.forEach(item => console.log(item));
+// colItems.forEach(item => console.log(item));
+for(let i = 0; i < colItems.length; i++){
+    console.log(colItems[i]);
+}
